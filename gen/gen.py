cf8d1836447e069022f4983f4e94775fd492d154
Eric Keller <erke9581@colorado.edu>
2022-05-19 11:34:31 -0600

    Added 2021 papers

--- gen/gen.py
+++ gen/gen.py
@@ -59,12 +59,12 @@ def main():
     lines = [x.strip('\n') for x in lines] 
 
   allentries = {}
-  years_covered = ["2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2004", "2003", "2002", "2001", "2000"]
+  years_covered = ["2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009", "2008", "2004", "2003", "2002", "2001", "2000"]
   for yr in years_covered:
      allentries[yr] = []
      
   for i in range(0, len(lines), 8):
-    #print(i)  
+    print(i)  
     year = lines[i].rstrip()
     entry = {}
     entry["title"] = lines[i+1].rstrip()
